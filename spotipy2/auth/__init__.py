0ad9ee8eabcdf32f826f9f8c5ac0ab7e953573ff
CyanBook <lorenzo.delmonte05@gmail.com>
2020-11-27 00:29:26 +0100

    Add __init__.py

---
+++ spotipy2/auth/__init__.py
@@ -0,0 +1,5 @@
+from .base_auth_flow import BaseAuthFlow
+from .client_credentials_flow import ClientCredentialsFlow
+from .token import Token
+
+__all__ = [BaseAuthFlow, ClientCredentialsFlow, Token]
