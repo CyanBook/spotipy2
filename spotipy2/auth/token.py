7b941aad58b7779cab89db6f5a8654ad056a3dea
CyanBook <lorenzo.delmonte05@gmail.com>
2020-11-27 00:28:22 +0100

    Add Token class - spotify token

---
+++ spotipy2/auth/token.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from datetime import datetime, timedelta
+
+
+class Token:
+    def __init__(
+        self,
+        access_token: str,
+        token_type: str,
+        scope: str,
+        expires_in: int,
+        refresh_token: Optional[str] = None
+    ) -> None:
+        self.access_token = access_token
+        self.token_type = token_type
+        self.scope = scope
+        self.expires_in = expires_in
+        self.refresh_token = refresh_token
+        self.expires_at = datetime.now() + timedelta(seconds=self.expires_in)
+
+    @classmethod
+    async def from_dict(cls, d: dict) -> "Token":
+        return cls(**d)
