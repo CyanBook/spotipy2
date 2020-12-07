f931789254360ec8d07b5aeb5548cd9957221631
CyanBook <lorenzo.delmonte05@gmail.com>
2020-12-07 19:55:38 +0100

    Parse everything outside __init__

--- spotipy2/auth/token.py
+++ spotipy2/auth/token.py
@@ -10,17 +10,27 @@ class Token:
         token_type: str,
         scope: str,
         expires_in: int,
+        expires_at: datetime,
         refresh_token: Optional[str] = None
     ) -> None:
         self.access_token = access_token
         self.token_type = token_type
         self.scope = scope
         self.expires_in = expires_in
-        self.expires_at = datetime.now(
-            timezone.utc
-        ) + timedelta(seconds=self.expires_in)
+        self.expires_at = expires_at
         self.refresh_token = refresh_token
 
     @classmethod
     async def from_dict(cls, d: dict) -> Token:
-        return cls(**d)
+        expires_at = datetime.now(
+            timezone.utc
+        ) + timedelta(seconds=d["expires_in"])
+
+        return cls(
+            access_token=d["access_token"],
+            token_type=d["token_type"],
+            scope=d["scope"],
+            expires_in=d["expires_in"],
+            expires_at=expires_at,
+            refresh_token=d["refresh_token"]
+        )
