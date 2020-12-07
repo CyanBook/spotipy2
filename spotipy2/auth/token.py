9a4f55027d887e99391d39c6f99f4a738b9f3f3e
CyanBook <lorenzo.delmonte05@gmail.com>
2020-12-07 19:57:15 +0100

    Change scope attribute to scopes. Space-separated string to list of string

--- spotipy2/auth/token.py
+++ spotipy2/auth/token.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional
+from typing import List, Optional
 from datetime import datetime, timedelta, timezone
 
 
@@ -8,14 +8,14 @@ class Token:
         self,
         access_token: str,
         token_type: str,
-        scope: str,
+        scopes: List[str],
         expires_in: int,
         expires_at: datetime,
         refresh_token: Optional[str] = None
     ) -> None:
         self.access_token = access_token
         self.token_type = token_type
-        self.scope = scope
+        self.scopes = scopes
         self.expires_in = expires_in
         self.expires_at = expires_at
         self.refresh_token = refresh_token
@@ -29,7 +29,7 @@ class Token:
         return cls(
             access_token=d["access_token"],
             token_type=d["token_type"],
-            scope=d["scope"],
+            scopes=d["scope"].split(),
             expires_in=d["expires_in"],
             expires_at=expires_at,
             refresh_token=d["refresh_token"]
