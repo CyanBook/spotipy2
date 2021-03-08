31a21b062062a0c6c494fb0a91d3a315e6aad4fa
CyanBook <cyan@cyanbook.dev>
2021-03-08 20:00:24 +0100

    Add support for spotify links

--- spotipy2/methods/__init__.py
+++ spotipy2/methods/__init__.py
@@ -1,6 +1,13 @@
+import re
+
 from .artists import ArtistMethods
 from .tracks import TrackMethods
 
 
 class Methods(ArtistMethods, TrackMethods):
-    pass
+    @staticmethod
+    def get_id(s: str) -> str:
+        if m := re.search("(?!.*/).+", s):
+            return m[0].split("?")[0]
+        else:
+            raise ValueError("ID not valid")
--- spotipy2/methods/tracks.py
+++ spotipy2/methods/tracks.py
@@ -10,12 +10,10 @@ class TrackMethods:
         self: spotipy2.Spotify,
         track_ids: List[str]
     ) -> List[Track]:
-        tracks = await self._get(
-            "tracks",
-            params={"ids": ",".join(track_ids)}
-        )
-
+        tracks = await self._get("tracks", params={"ids": ",".join(track_ids)})
         return [await Track.from_dict(track) for track in tracks["tracks"]]
 
     async def get_track(self: spotipy2.Spotify, track_id: str) -> Track:
-        return await Track.from_dict(await self._get(f"tracks/{track_id}"))
+        return await Track.from_dict(
+            await self._get(f"tracks/{self.get_id(track_id)}")
+        )
