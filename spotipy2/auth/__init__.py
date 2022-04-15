0f3fc429fcd83a30e457318df4f86c4534e9c220
Coddo <coddobusiness@gmail.com>
2022-04-15 15:06:10 +0400

    Add Authorization Code Flow

--- spotipy2/auth/__init__.py
+++ spotipy2/auth/__init__.py
@@ -1,5 +1,6 @@
 from .base_auth_flow import BaseAuthFlow
 from .client_credentials_flow import ClientCredentialsFlow
+from .oauth_flow import OauthFlow
 from .token import Token
 
-__all__ = ["BaseAuthFlow", "ClientCredentialsFlow", "Token"]
+__all__ = ["BaseAuthFlow", "ClientCredentialsFlow", "Token", "OauthFlow"]
