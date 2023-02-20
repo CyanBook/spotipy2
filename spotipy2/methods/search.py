8cfa1c200b42834ba2e004ac3c3e685545c4cbec
Lorenzo Delmonte <cyan@cyanbook.dev>
2023-02-20 18:45:39 +0100

    Change search return behaviour
    If only a search type is queried it now returns a Paging object, instead of a dict with one key-value pair.

--- spotipy2/methods/search.py
+++ spotipy2/methods/search.py
@@ -6,12 +6,12 @@ from spotipy2.types import Paging, Album, Artist, Playlist, Track
 
 
 class SearchMethods:
-    TYPES_SUPPORTED = [Album, Artist, Playlist, Track]
+    SEARCH_TYPES_SUPPORTED = [Album, Artist, Playlist, Track]
 
     async def search(
         self: spotipy2.Spotify,  # type: ignore
         query: str,
-        types: Union[TYPES_SUPPORTED, List[TYPES_SUPPORTED]],
+        types: Union[SEARCH_TYPES_SUPPORTED, List[SEARCH_TYPES_SUPPORTED]],
         market: Optional[str] = None,
         limit: Optional[int] = None,
         offset: Optional[int] = None,
@@ -23,10 +23,16 @@ class SearchMethods:
             market=market, limit=limit, offset=offset, include_external=include_external
         )
 
-        return await self._get(
+        
+        r = await self._get(
             "search",
             params={
                 **{"query": query, "type": ",".join(t.__name__.lower() for t in types)},
                 **params,
             },
         )
+
+        if len(r) == 1:
+            return next(iter(r.values()))
+
+        return r
